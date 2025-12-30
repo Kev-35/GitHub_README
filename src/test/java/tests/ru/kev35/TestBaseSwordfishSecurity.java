1f11963c1805d63b09c160d14020412698a720ab
Kuznetsov_Evgeny <kev35.vol@gmail.com>
2025-12-30 18:27:22 +0300

    remark

--- src/test/java/tests/ru/kev35/TestBaseSwordfishSecurity.java
+++ src/test/java/tests/ru/kev35/TestBaseSwordfishSecurity.java
@@ -19,7 +19,7 @@ public class TestBaseSwordfishSecurity {
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
- //       Configuration.remote = ("https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = ("https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.baseUrl = "https://swordfish-security.ru/";
         Configuration.pageLoadStrategy = "eager";
         DesiredCapabilities capabilities = new DesiredCapabilities();
